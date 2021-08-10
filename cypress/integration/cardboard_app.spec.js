006bba2dafaf46a00ede88a0259ea85db19544af
Jakub Wąsowski <jakub.wasowski@packhelp.com>
2021-08-10 16:27:25 +0200

    fix: removed cypress

--- cypress/integration/cardboard_app.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Cardboard app', () => {
-  it('successfully loads application and fetches cards', () => {
-    cy.intercept('GET', '/cards', { fixture: 'cards.json' })
-
-    cy.visit('/')
-  })
-})
